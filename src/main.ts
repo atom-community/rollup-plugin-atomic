3de9d1b4ae3a3dc1973bc86bdfd24e92c4b12718
aminya <aminyahyaabadi74@gmail.com>
2020-07-23 17:14:50 -0500

    major: support giving options
    BREAKING CHANGE

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import includesAny from "array-includes-any";
-
 // common plugins
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
@@ -48,72 +46,130 @@ function includesAny(
   }
   return null;
 }
+
 export function createPlugins(
-  languages: Array<string> = ["ts", "js", "json", "coffee"],
-  babel: boolean = true,
-  extraPlugins?: Array<any>
+  inputPluginsNames: Array<Plugin> = ["ts", "js", "json", "coffee"],
+  extraPlugins?: Array<any> | boolean,
+  extraPluginsDeprecated?: Array<any>
 ) {
-  let plugins = []
+  let plugins = [];
 
   // language specific
+
   // typescript
-  if (includesAny(languages, ["ts", ".ts", "typescript", "TypeScript"])) {
+  const tsIndex = includesAny(inputPluginsNames, [
+    "ts",
+    ".ts",
+    "typescript",
+    "TypeScript",
+  ]);
+  if (tsIndex !== null) {
     const typescript = require("@rollup/plugin-typescript");
-    plugins.push(
-      typescript({
-        noEmitOnError: false,
-      })
-    );
+    if (typeof inputPluginsNames[tsIndex] === "string") {
+      // plugin name only
+      plugins.push(
+        typescript({
+          noEmitOnError: false,
+        })
+      );
+    } else {
+      // plugin with options
+      plugins.push(typescript(inputPluginsNames[tsIndex][1]));
+    }
   }
+
   // coffeescript
-  if (
-    includesAny(languages, [
-      "coffee",
-      ".coffee",
-      "coffeescript",
-      "coffee-script",
-      "CoffeeScript",
-    ])
-  ) {
+  const coffeeIndex = includesAny(inputPluginsNames, [
+    "coffee",
+    ".coffee",
+    "coffeescript",
+    "coffee-script",
+    "CoffeeScript",
+    "cs",
+  ]);
+  if (coffeeIndex !== null) {
     const coffeescript = require("rollup-plugin-coffee-script");
-    plugins.push(coffeescript());
+    if (typeof inputPluginsNames[coffeeIndex] === "string") {
+      // plugin name only
+      plugins.push(coffeescript());
+    } else {
+      // plugin with options
+      plugins.push(coffeescript(inputPluginsNames[coffeeIndex][1]));
+    }
   }
+
   // json
-  if (includesAny(languages, ["json", ".json", "JSON"])) {
+  const jsonIndex = includesAny(inputPluginsNames, ["json", ".json", "JSON"]);
+  if (jsonIndex !== null) {
     const json = require("@rollup/plugin-json");
-    plugins.push(json({ compact: true }));
+    if (typeof inputPluginsNames[jsonIndex] === "string") {
+      // plugin name only
+      plugins.push(json({ compact: true }));
+    } else {
+      // plugin with options
+      plugins.push(json(inputPluginsNames[jsonIndex][1]));
+    }
   }
 
   // css only
-  if (includesAny(languages, ["css", ".css"])) {
+  const cssIndex = includesAny(inputPluginsNames, ["css", ".css"]);
+  if (cssIndex !== null) {
+    const cssOnly = require("rollup-plugin-css-only");
     console.log(`
       css only was chosen to bundle css files into a single file.
       This plugin requires you to import css files in a dummy js file and pass it as an input to rollup.
       This should be done in a separate step from src code bundling
     `);
-    const cssOnly = require("rollup-plugin-css-only");
-    plugins.push(cssOnly({ output: "dist/bundle.css" }));
+    if (typeof inputPluginsNames[cssIndex] === "string") {
+      // plugin name only
+      plugins.push(cssOnly({ output: "dist/bundle.css" }));
+    } else {
+      // plugin with options
+      plugins.push(cssOnly(inputPluginsNames[cssIndex][1]));
+    }
     // minify css
     if (process.env.NODE_ENV === "production") {
+      // TODO get the output from the plugin when the user uses options
       const execute = require("rollup-plugin-execute");
       plugins.push(execute(["csso dist/bundle.css --output dist/bundle.css"]));
     }
   }
 
-  // babel for js and coffee
-  if (babel || languages.includes("babel")) {
-    const { babel } = require("@rollup/plugin-babel");
-    plugins.push(
-      babel({
-        extensions: [".js", ".jsx", ".mjs", ".coffee"],
-        babelHelpers: "bundled",
-      })
+  // babel
+  let babelInput = extraPlugins
+  if (typeof babelInput === "boolean") {
+    console.warn(
+      'Setting babel with the second argument is depcrated. Pass "babel" like other plugins to the first argument'
     );
   }
 
+  const babelIndex = includesAny(inputPluginsNames, ["babel"]);
+  if (babelIndex !== null || babelInput === true) {
+    const { babel } = require("@rollup/plugin-babel");
+    if (
+      babelInput === true ||
+      typeof inputPluginsNames[babelIndex!] === "string"
+    ) {
+      // plugin name only
+      plugins.push(
+        babel({
+          extensions: [".js", ".jsx", ".mjs", ".coffee"],
+          babelHelpers: "bundled",
+        })
+      );
+    } else {
+      // plugin with options
+      plugins.push(babel(inputPluginsNames[babelIndex!][1]));
+    }
+  }
+
   // extra plugins
-  if (extraPlugins) {
+  if (typeof extraPlugins !== "boolean" && extraPlugins !== undefined) {
     plugins.push(...extraPlugins);
+  }
+
+  if (extraPluginsDeprecated) {
+    plugins.push(...extraPluginsDeprecated);
   }
 
   let pluginsCommon = [
@@ -133,7 +189,7 @@ export function createPlugins(
     commonjs(),
   ];
 
-  plugins.push(...pluginsCommon)
+  plugins.push(...pluginsCommon);
 
   // minify only in production mode
   if (process.env.NODE_ENV === "production") {
